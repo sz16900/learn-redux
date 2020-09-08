15c478c7444ac8e68cd97a00f946dc9da94e412e
sz16900 <sz16900@my.bristol.ac.uk>
2020-09-08 05:35:27 -1000

    trying out the redux thingy

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// STORE -> Globalized State
+
+// ACTION
+const increment = () => ({
+  type: 'INCREMENT',
+});
+
+const decrement = () => ({
+  type: 'DECREMENT',
+});
+
+// REDUCER -> Transform the state into the next state
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// display to console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
